aedab1f37a41422b018840a67f2bb8d0b9b74b5d
Moose <gabrielmarianoofficial@gmail.com>
2022-09-13 13:30:20 +0800

    first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://gab:gab123@cluster0.owcv8h6.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup Inserted!" });
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+
+export default function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://gab:gab123@cluster0.owcv8h6.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
